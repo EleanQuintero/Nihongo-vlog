4a0e05ed90ef214a0df2648482abfd6569bd3b41
Elean Quintero <167714130+EleanQuintero@users.noreply.github.com>
2024-10-29 00:55:52 +0100

    Agrega MobileNavBar y ajusta el Header para mejorar la navegación en dispositivos móviles, integrando dinámicamente las secciones

---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+export const URL_SECTIONS ={
+    Inicio: {
+        title: "Inicio",
+        url: "/"
+    },
+    Autodidacta: {
+        title: "Autodidacta",
+        url: "/autodidacta"
+    },
+    Libros: {
+        title: "Libros recomendados",
+        url: "/books-recom"
+    },
+    Cursos: {
+        title: "Cursos",
+        url: "/cursos"
+    },
+    Consejos: {
+        title: "Consejos",
+        url: "/consejos"
+    },
+    About: {
+        title: "About",
+        url: "/nihongo-blog-about"
+    }
+}
